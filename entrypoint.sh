2ac0dfdb85aed36e68c2020fb162a76f3049b09d
Ladet tanguy <47148093+ladettanguy@users.noreply.github.com>
2022-03-28 10:10:31 +0200

    Feature/logger (#1)
    * logging module installed for debug log description
    
    * set logging level to INFO
    
    * entrypoint, change the log.txt delete to log.log
    
    Co-authored-by: tladet <tladet@reseau.free.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 source  venv/bin/activate > /dev/null
 pip install -r requirements.txt > /dev/null
-rm -f log.txt
+rm -f log.log
 python3 client.py > /dev/null &
 
 echo "Le client discord ce lance"
