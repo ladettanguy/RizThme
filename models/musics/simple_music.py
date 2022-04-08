6c3e5bed69f81725df1f71c4d66034a523cab70d
tladet <tladet@reseau.free.fr>
2022-04-08 08:23:25 +0200

    New abstraction Playable.

--- models/musics/simple_music.py
+++ models/musics/simple_music.py
@@ -1,53 +1,11 @@
-from abc import ABC, abstractmethod
-from typing import Optional, Callable
-
-import discord
+from abc import ABC
 
 from .audio_item import AudioItem
+from .playable import Playable
 
 
-class SimpleMusic(AudioItem, ABC):
+class SimpleMusic(AudioItem, Playable, ABC):
     """
     Abstract class for music.
     """
-
-    @abstractmethod
-    def is_valid(self, send_message: bool = False) -> bool:
-        """
-        Check if the Music instance is correctly playable
-        :param send_message: bool, Optional argument for send error message in message's channel with the error stack
-        :return: True if the music is valid and playable
-        """
-        pass
-
-    @abstractmethod
-    def get_audio_source(self) -> discord.AudioSource:
-        """
-        Create a discord.AudioSource from the init music link
-        :return: discord.AudioSource usable like AudioSource for discord.VoiceClient
-        """
-        pass
-
-    @abstractmethod
-    def get_duration(self) -> int:
-        """
-        :return: duration of the music in seconds
-        """
-        pass
-
-    @abstractmethod
-    def play(self, voice_client: discord.VoiceClient, after: Optional[Callable] = None):
-        """
-        Play the sound in the voice_client and execute the callback function @after when it's finish
-        :param voice_client: discord.VoiceClient
-        :param after: Optional[Callable]
-        """
-        pass
-
-    @abstractmethod
-    def stop(self, voice_client: discord.VoiceClient):
-        """
-        Stop the music in the voice_client
-        :param voice_client: discord.VoiceClient
-        """
-        pass
+    pass
