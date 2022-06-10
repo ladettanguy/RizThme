d4347217237141d128a7375b5e5879748fa88a3a
tamikata <sti2dlab.ladettanguy@gmail.com>
2022-06-10 13:52:22 +0200

    fix

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ from .setting import CLIENT, TOKEN
 logging.basicConfig(filename='log.log', filemode='w', level="INFO",
                     format='%(asctime)s -- %(name)s  -- %(levelname)s -- %(message)s', force=True)
 
-CLIENT.run(TOKEN)
+CLIENT.run(TOKEN, loop=asyncio.get_event_loop())
