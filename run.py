99bed1ea4682d26d6c5e45517d39c508fff10228
tamikata <sti2dlab.ladettanguy@gmail.com>
2022-06-10 12:00:39 +0200

    add the connect method

--- run.py
+++ run.py
@@ -13,4 +13,5 @@ from .setting import CLIENT, TOKEN
 logging.basicConfig(filename='log.log', filemode='w', level="INFO",
                     format='%(asctime)s -- %(name)s  -- %(levelname)s -- %(message)s', force=True)
 
+CLIENT.connect()
 asyncio.run(CLIENT.login(TOKEN))
